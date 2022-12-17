4350f3cecd374af13cb7e97a44f7ee1518d5aad7
bentleyjensen <bentleyjensen@users.noreply.github.com>
2022-12-16 18:49:32 -0800

    add alerts

--- src/EventCount.js
+++ src/EventCount.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from "./Alert";
 
 class EventCount extends Component {
+    state = {
+        errMsg: '',
+    }
+
     // Keep in mind that this event arg is the 'change' event, not a calendar event
     handleOnChange = (event) => {
-        const num = parseInt(event.target.value);
-        if (num > 0) {
-            this.props.updateCount(num)
-        } else {
-            this.props.updateCount(0)
+        if (event.target.value === '') {
+            this.setState({ errMsg: '' });
+            this.props.updateCount('');
+            return;
         }
+        const num = parseInt(event.target.value || 0);
+        const numInRange = num > 0 && num <= 64;
+
+        if (!num || !numInRange) {
+            this.setState({ errMsg: 'Please choose a number between 1 and 64' });
+        } else {
+            this.setState({ errMsg: '' });
+        }
+        this.props.updateCount(num);
     }
 
     render() {
-        const displayValue = (this.props.countEvents === 0) ? '' : this.props.countEvents;
-        return <input
-            className='eventCount'
-            value={displayValue}
-            onChange={this.handleOnChange}
-            onFocus={(event) => event.target.select()} // Highlight text on focus
-        ></input>;
+        const displayValue = this.props.countEvents || '';
+        return <>
+            <input
+                className='eventCount'
+                value={displayValue}
+                onChange={this.handleOnChange}
+                onFocus={(event) => event.target.select()} // Highlight text on focus
+            ></input>
+            <ErrorAlert text={this.state.errMsg} />
+        </>;
     }
 }
 export default EventCount;
